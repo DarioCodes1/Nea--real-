18d847c785738e163b858ac14a48033a751976da
dariocodes1 <you@example.com>
2023-12-18 21:34:08 +0000

    Created simple window and 3 functions for updating the Database

---
+++ loginpage.py
@@ -0,0 +1,30 @@
+import tkinter
+from tkinter import messagebox
+import sqlite3 
+
+window = tkinter.Tk()
+window.title('Login Page')
+window.geometry('750x500')
+window.configure(bg='#467D6F')
+
+def createdatabase():
+    conn = sqlite3.connect('Login details.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+                   CREATE TABLE IF NOT EXISTS users(
+                       id INTEGER PRIMARY KEY AUTOINCREMENT
+                       username TEXT NOT NULL,
+                       password TEXT NOT NULL
+                    )
+        ''')
+    conn.commit()
+    conn.close()
+                
+def new_user(username, password):
+    conn = sqlite3.connect("Login details.db")
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users(username, password) VALUES (?,?)',(username, password))
+    conn.commit()
+    conn.close()
+
+window.mainloop()
