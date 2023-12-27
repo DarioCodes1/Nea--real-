908a0d868c2fd989b86e4385bf5d8e06a373c60f
dariocodes1 <you@example.com>
2023-12-27 13:43:12 +0000

    Made frame the parent class rather than window, for all of the widgets and have also "packed" the widgets into the centre of the window. There is still an issue with the background colour of the frame, but I will fix that later on.

--- loginpage.py
+++ loginpage.py
@@ -7,13 +7,15 @@ window.title('Login Page') #Title the Window
 window.geometry('750x500') #Give the dimensions of the desired window
 window.configure(bg='#467D6F') #Give the background a colour
 
+frame = tkinter.Frame()
+
 #Creating widgets
-login_label = tkinter.Label(window, text="Login", bg='#467D6F', fg="#FFFFFF", font=("Times New Roman", 25), pady=25)
-username_label = tkinter.Label(window, text="Username", bg='#467D6F', fg="#FFFFFF", font=("Times New Roman", 12) ,pady=5)
-username_entry = tkinter.Entry(window, font=("Times New Roman", 12))
-password_label = tkinter.Label(window, text="Password", bg='#467D6F', fg="#FFFFFF", font=("Times New Roman", 12), pady=5)
-password_entry = tkinter.Entry(window, show="*", font=("Times New Roman", 12))
-button = tkinter.Button(window, text="Login", bg="#AABF11", fg="#FFFFFF", font=("Times New Roman", 10), pady=5)
+login_label = tkinter.Label(frame, text="Login", bg='#467D6F', fg="#AABF11", font=("Times New Roman", 25), pady=25)
+username_label = tkinter.Label(frame, text="Username", bg='#467D6F', fg="#FFFFFF", font=("Times New Roman", 12) ,pady=5)
+username_entry = tkinter.Entry(frame, font=("Times New Roman", 12))
+password_label = tkinter.Label(frame, text="Password", bg='#467D6F', fg="#FFFFFF", font=("Times New Roman", 12), pady=5)
+password_entry = tkinter.Entry(frame, show="*", font=("Times New Roman", 12))
+button = tkinter.Button(frame, text="Login", bg="#AABF11", fg="#FFFFFF", font=("Times New Roman", 10), pady=5)
 
 #Placing the widgets on the page
 login_label.grid(row=0, column=0, columnspan=2)
@@ -22,6 +24,8 @@ username_entry.grid(row=1, column=1)
 password_label.grid(row=2, column=0)
 password_entry.grid(row=2, column=1)
 button.grid(row=3, column=0, columnspan =2)
+
+frame.pack()
 
 
 def createdatabase():
