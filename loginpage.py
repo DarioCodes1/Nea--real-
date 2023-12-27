055df51ca37d353828ee4c6884820a3c2df8d1ae
dariocodes1 <you@example.com>
2023-12-27 12:08:20 +0000

    Added widgets for labels and entry windows and diplayed them. Also added comments.

--- loginpage.py
+++ loginpage.py
@@ -2,30 +2,47 @@ import tkinter
 from tkinter import messagebox
 import sqlite3 
 
-window = tkinter.Tk()
-window.title('Login Page')
-window.geometry('750x500')
-window.configure(bg='#467D6F')
+window = tkinter.Tk() #Initialise the window
+window.title('Login Page') #Title the Window
+window.geometry('750x500') #Give the dimensions of the desired window
+window.configure(background='#467D6F') #Give the background a colour
+
+#Creating widgets
+login_label = tkinter.Label(window, text="Login", background='#467D6F')
+username_label = tkinter.Label(window, text="Username", background='#467D6F')
+username_entry = tkinter.Entry(window)
+password_label = tkinter.Label(window, text="Password", background='#467D6F')
+password_entry = tkinter.Entry(window)
+button = tkinter.Button(window, text="Login")
+
+#Placing the widgets on the page
+login_label.grid(row=0, column=0, columnspan=2)
+username_label.grid(row=1, column=0)
+username_entry.grid(row=1, column=1)
+password_label.grid(row=2, column=0)
+password_entry.grid(row=2, column=1)
+button.grid(row=3, column=0, columnspan =2)
+
+
 
 def createdatabase():
-    conn = sqlite3.connect('Login details.db')
-    cursor = conn.cursor()
+    conn = sqlite3.connect('Login details.db') 
+    cursor = conn.cursor() 
     cursor.execute('''
                    CREATE TABLE IF NOT EXISTS users(
                        id INTEGER PRIMARY KEY AUTOINCREMENT
                        username TEXT NOT NULL,
                        password TEXT NOT NULL
                     )
-        ''')
-    conn.commit()
-    conn.close()
+        ''') #Create database 
+    conn.commit() #Save changes to database
+    conn.close() #Close database 
                 
 def new_user(username, password):
-    conn = sqlite3.connect("Login details.db")
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO users(username, password) VALUES (?,?)',(username, password))
-    conn.commit()
-    conn.close()
-    print("hello")
+    conn = sqlite3.connect("Login details.db") 
+    cursor = conn.cursor() 
+    cursor.execute('INSERT INTO users(username, password) VALUES (?,?)',(username, password)) #Add a new username and password for a new user
+    conn.commit() #Save changes to database
+    conn.close() #Close database
 
-window.mainloop()
+window.mainloop() #Refresh the page (run the code)
