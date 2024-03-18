edaeaed6c69905ec6d16cc56d3d29d450a5a404b
dariocodes1 <you@example.com>
2024-03-18 23:33:51 +0000

    created open_camera_display() subroutine Uses cv2 and most of this subroutine is from: https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html

---
+++ display.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+def open_camera_display():
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    
+    while True:
+        ret, frame = cap.read()  # Capture frame-by-frame
+    
+        # Display the resulting frame
+        cv.imshow('frame', frame)
+        
+        if cv.waitKey(1) == ord('q'):
+            break
+
+    cap.release()  # When everything done, release the capture
+    cv.destroyAllWindows()
+
+open_camera_display()
